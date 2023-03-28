655a769ef2974f7187c4c1e2dea4cbfd01192174
dezoliveira <andresoliveira@protonmail.com>
2023-03-28 16:09:53 -0300

    Added Image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <v-app :style="{ background: $vuetify.theme.themes.dark.background }">
     <Navbar />
+    <v-container fluid>
+      <v-col cols="6">
+        <v-img src="1.jpg" contain max-height="500"></v-img>
+      </v-col>
+      <v-col cols="6">
+        <h5 class="red--text text-darken-4"></h5>
+      </v-col>
+    </v-container>
   </v-app>
 </template>
 

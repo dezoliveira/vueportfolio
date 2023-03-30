6b66060a60289c733ab06b907821d424c0860e5c
dezoliveira <andresoliveira@protonmail.com>
2023-03-30 16:22:52 -0300

    Contact and Footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -161,19 +161,51 @@
             <v-icon right>mdi-arrow-right</v-icon>
           </v-btn>
         </v-col>
+        <v-col cols="12" class="padding topInverse">
+          <div class="second">
+            <div class="secondchild1" id="contact">
+              <v-row>
+                <v-col cols="7">
+                  <h1 class="white--text">
+                    Let's work together on<br/>your next project
+                  </h1>
+                  <p class="grey--text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Elius nesciunt ducimilis natus
+                  </p>
+                </v-col>
+                <v-col cols="5">
+                  <v-btn tile color="#A7121D" dark class="mt-16">
+                    Contact
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+          <v-toolbar class="topToolbar" color="#111111" dark flat>
+            <div style="position:absolute; margin-left: auto; margin-right: auto; left: 0; right: 0; text-align: center;">
+              <v-btn text>Home</v-btn>
+              <v-btn text>Project</v-btn>
+              <v-btn text>About</v-btn>
+              <v-btn text>Contact</v-btn>
+            </div>
+          </v-toolbar>
+        </v-col>
       </v-row>
     </v-container>
+    <Footer />
   </v-app>
 </template>
 
 <script>
   import Navbar from '../components/Navbar'
+  import Footer from '../components/Footer'
 
   export default {
     name: 'Home',
 
     components: {
       Navbar,
+      Footer
     },
   }
 </script>
